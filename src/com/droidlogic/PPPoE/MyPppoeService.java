0aeed21fc296df44f8c0f945bed1c126709ab4da
Zhiwei Gong <zhiwei.gong@amlogic.com>
2015-04-30 15:04:02 +0800

    PD#106222:fix PPPoE can not automatic access network when plug
    Change-Id: I81cee783447f28a0090db6e09d473c98def9557c

--- src/com/droidlogic/PPPoE/MyPppoeService.java
+++ src/com/droidlogic/PPPoE/MyPppoeService.java
@@ -15,6 +15,11 @@ import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import com.amlogic.pppoe.PppoeOperation;
+import com.android.server.net.BaseNetworkObserver;
+import android.os.INetworkManagementService;
+import android.os.ServiceManager;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
 
 
 public class MyPppoeService extends Service
@@ -23,15 +28,24 @@ public class MyPppoeService extends Service
     private NotificationManager mNM;
     private Handler mHandler;
     private PppoeOperation operation = null;
-
+    private InterfaceObserver mInterfaceObserver;
+    private INetworkManagementService mNMService;
 
     @Override
     public void onCreate() {
         Log.d(TAG, ">>>>>>onCreate");
+        IBinder b = ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE);
+        mNMService = INetworkManagementService.Stub.asInterface(b);
 
         mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         mHandler = new DMRHandler();
+        mInterfaceObserver = new InterfaceObserver();
+        try {
+            mNMService.registerObserver(mInterfaceObserver);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Could not register InterfaceObserver " + e);
+        }
 
         /* start check after 5s */
         mHandler.sendEmptyMessageDelayed(0, 5000);
@@ -40,8 +54,8 @@ public class MyPppoeService extends Service
 
         f.addAction(Intent.ACTION_SHUTDOWN);
         f.addAction(Intent.ACTION_SCREEN_OFF);
+        f.addAction(Intent.ACTION_SCREEN_ON);
         registerReceiver(mShutdownReceiver, new IntentFilter(f));
-
     }
 
     @Override
@@ -55,7 +69,24 @@ public class MyPppoeService extends Service
     public IBinder onBind(Intent intent) {
         return null;
     }
+    private void updateInterfaceState(String iface, boolean up) {
+        Intent intent = new Intent("com.droidlogic.linkchange");
 
+        if (!iface.contains("eth0")) {
+            return;
+        }
+        if (up) {
+            this.sendBroadcast(intent);
+        }
+    }
+
+    private class InterfaceObserver extends BaseNetworkObserver {
+        @Override
+        public void interfaceLinkStateChanged(String iface, boolean up) {
+            updateInterfaceState(iface, up);
+        }
+
+    }
 
     private class DMRHandler extends Handler
     {
@@ -77,9 +108,13 @@ private BroadcastReceiver mShutdownReceiver =
         Log.d(TAG , "onReceive :" +intent.getAction());
 
         if ((Intent.ACTION_SCREEN_OFF).equals(intent.getAction())) {
-                operation = new PppoeOperation();
-                operation.disconnect();
+            operation = new PppoeOperation();
+            operation.disconnect();
         }
+        if ((Intent.ACTION_SCREEN_ON).equals(intent.getAction())) {
+            updateInterfaceState("eth0", true);
+        }
+
     }
 };
 
